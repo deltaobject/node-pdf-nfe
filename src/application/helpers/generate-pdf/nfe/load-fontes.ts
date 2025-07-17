bdd8a32cf55e8604f2b0a9d51ead4587f43b82e2
DeltaObject <cassiosantini@gmail.com>
2025-07-16 23:12:54 -0300

    fonts_pdfkit

--- src/application/helpers/generate-pdf/nfe/load-fontes.ts
+++ src/application/helpers/generate-pdf/nfe/load-fontes.ts
@@ -1,8 +1,5 @@
 // src/application/helpers/generate-pdf/nfe/load-fontes.ts
 
-/**
- * Mapeia nomes lógicos para fontes padrão do PDFKit (Times-Roman).
- */
 export function loadFonts(doc: PDFKit.PDFDocument): void {
   doc.registerFont('normal', 'Times-Roman');
   doc.registerFont('negrito', 'Times-Bold');
@@ -10,9 +7,6 @@ export function loadFonts(doc: PDFKit.PDFDocument): void {
   doc.registerFont('negrito-italico', 'Times-BoldItalic');
 }
 
-/**
- * Para NFC-e, usa Helvetica como alternativa leve e moderna.
- */
 export function loadFontsNFCe(doc: PDFKit.PDFDocument): void {
   doc.registerFont('normal', 'Helvetica');
   doc.registerFont('negrito', 'Helvetica-Bold');
